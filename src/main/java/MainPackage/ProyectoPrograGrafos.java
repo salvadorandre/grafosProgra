3d91749fb0792e898fbf8a3d98a3679398ba7f4b
Allan Lacan <allanvasquez1312@gmail.com>
2026-05-27 23:38:29 -0600

    feat: cambios chidos

--- src/main/java/MainPackage/ProyectoPrograGrafos.java
+++ src/main/java/MainPackage/ProyectoPrograGrafos.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package MainPackage;
 
-/**
- *
- * @author allan
- */
+
 public class ProyectoPrograGrafos {
 
     public static void main(String[] args) {
